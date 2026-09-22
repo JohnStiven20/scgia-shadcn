1c609d10259ec38d632a51757715d1ea2b721d33
JohnStiven20 <stivensolanomacas@gmail.com>
2026-09-22 19:24:26 +0200

    fix: append "/api" to configuredApiBaseUrl for correct API endpoint

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -3,6 +3,6 @@
 //     ? `${window.location.protocol}//${window.location.hostname}:8083/api`
 //     : "http://localhost:8083/api"
 
-const configuredApiBaseUrl = import.meta.env.VITE_API_URL
+const configuredApiBaseUrl    =  import.meta.env.VITE_API_URL  + "/api"
 
 export const API_BASE_URL = configuredApiBaseUrl
